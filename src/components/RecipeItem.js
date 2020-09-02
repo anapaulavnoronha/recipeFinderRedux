ca78a83f10d9eb28080acf4e3bebec6cc0d68080
Ana <a.vargas.de.noronha@accenture.com>
2020-09-03 00:44:38 +0100

    Adding favorite recipe function to RecipeItem

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { favoriteRecipe } from "../actions";
 
 class RecipeItem extends Component {
+  constructor() {
+    super();
+    this.state = { favorite: false };
+  }
+
+  favorite(recipe) {
+    console.log("recipe");
+    this.props.favoriteRecipe(recipe);
+    this.setState({ favorite: true });
+  }
   render() {
     let { recipe } = this.props;
     return (
       <div className="recipe-item">
+        {this.state.favorite ? (
+          <div className="star"> &#9733;</div>
+        ) : (
+          <div className="star" onClick={() => this.favorite(recipe)}>
+            &#9734;
+          </div>
+        )}
+
         <div className="recipe-text">
           <a href={recipe.href}>
             <h4>{recipe.title}</h4>
@@ -21,4 +41,7 @@ class RecipeItem extends Component {
   }
 }
 
-export default RecipeItem;
+export default connect(
+  null,
+  { favoriteRecipe }
+)(RecipeItem);
